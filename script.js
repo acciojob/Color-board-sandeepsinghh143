44f3b942ea0ac9aeb9e63c7025d4bd25a8cf0df8
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-04-24 13:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+// Dynamically render 800 boxes
+for (let i = 0; i < 800; i++) {
+  const box = document.createElement('div');
+  box.classList.add('square');
+  container.appendChild(box);
+}
+
+// Add event listeners to change color on hover
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'red';
+  });
+  square.addEventListener('mouseout', () => {
+    setTimeout(() => {
+      square.style.backgroundColor = 'white';
+    }, 1000);
+  });
+});
